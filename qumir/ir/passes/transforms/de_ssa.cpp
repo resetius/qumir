db8b7f2a8f535a6cbdb08c42ec35895209d63935
Alexey Ozeritskiy <aozeritsky@gmail.com>
2025-10-22 00:28:33 +0100

    Fix critical edges

--- qumir/ir/passes/transforms/de_ssa.cpp
+++ qumir/ir/passes/transforms/de_ssa.cpp
@@ -9,14 +9,53 @@ namespace NPasses {
 using namespace NLiterals;
 
 void DeSSA(TFunction& function, TModule& module) {
+    std::vector<TBlock> newBlocks;
+    std::map<std::pair<TLabel,TLabel>, TLabel> remap;
+    // First, split critical edges
     for (auto& block : function.Blocks) {
-        for (auto predLabel : block.Pred) {
+        for (auto& predLabel : block.Pred) {
             auto& predBlock = function.Blocks[function.GetBlockIdx(predLabel)];
             if (predBlock.Succ.size() > 1 && block.Pred.size() > 1) {
                 // critical edge, need to split
-                throw std::runtime_error("DeSSA: critical edge detected, splitting not implemented yet");
+                TLabel newLabel = {function.NextLabelIdx++};
+                newBlocks.push_back(TBlock {
+                    .Label = newLabel,
+                    .Instrs = {
+                        {
+                            .Op = "jmp"_op,
+                            .Operands = {block.Label},
+                            .OperandCount = 1,
+                        }
+                    },
+                    .Pred = {predLabel},
+                    .Succ = {block.Label}
+                });
+                remap[{predLabel, block.Label}] = newLabel;
+                auto& termInstr = predBlock.Instrs.back();
+                for (size_t i = 0; i < termInstr.OperandCount; ++i) {
+                    if (termInstr.Operands[i] == block.Label) {
+                        termInstr.Operands[i] = newLabel;
+                    }
+                }
+                for (auto& succLabel : predBlock.Succ) {
+                    if (succLabel == block.Label) {
+                        succLabel = newLabel;
+                    }
+                }
+                predLabel = newLabel;
             }
         }
+    }
+    for (const auto& newBlock : newBlocks) {
+        auto idx = function.Blocks.size();
+        function.Blocks.push_back(newBlock);
+        if (newBlock.Label.Idx >= function.Label2Idx.size()) {
+            function.Label2Idx.resize(newBlock.Label.Idx + 1, -1);
+        }
+        function.Label2Idx[newBlock.Label.Idx] = idx;
+    }
+
+    for (auto& block : function.Blocks) {
         for (const auto& phi : block.Phis) {
             if (phi.Op != "phi"_op) {
                 // skip nops
@@ -25,6 +64,10 @@ void DeSSA(TFunction& function, TModule& module) {
             for (size_t i = 0; i < phi.Operands.size(); i += 2) {
                 auto value = phi.Operands[i];
                 auto label = phi.Operands[i + 1].Label;
+                auto maybeRemapIt = remap.find({label, block.Label});
+                if (maybeRemapIt != remap.end()) {
+                    label = maybeRemapIt->second;;
+                }
                 auto& predBlock = function.Blocks[function.GetBlockIdx(label)];
                 auto op = (value.Type == TOperand::EType::Tmp)
                     ? "mov"_op
